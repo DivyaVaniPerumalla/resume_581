c82483b30ca110373b7cc494b8091d6327c43559
divyavani123 <divyaperumalla7@gmail.com>
2018-03-29 08:42:14 +0530

    modified javascript

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ loadjson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
 basics(data.left);
- // let data1=JSON.parse(text);
- // console.log(data1);
- // basics1(data1.right);
+  let data1=JSON.parse(text);
+  console.log(data1);
+education(data1.education);
 })
 
 // for main div class calling
@@ -29,20 +29,26 @@ var main=document.getElementById('main');
 // element creation
 var left=document.createElement("div");
 // add class name to element
-left.classList.add("left");
+ left.classList.add("left");
 // add id to Element
-left.setAttribute("id","left");
+ left.setAttribute("id","left");
 // adding text to divyaperumalla
-left.textContent="Profile Details:"
-left.appendChild(document.createElement("HR"));
+ left.textContent="Profile Details:"
+ left.appendChild(document.createElement("HR"));
 // appending to main div
 main.appendChild(left);
-// var right=document.createElement("div");
-// right.classList.add("right");
-// right.setAttribute("id","right");
 
+var right=document.createElement("div");
+right.classList.add("right");
+ right.setAttribute("id","right");
 
-
+main.appendChild(right);
+// education div start
+var edu=document.createElement("div");
+edu.classList.add("edu1");
+edu.textContent="Education Details:";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
 
 function basics(leftside){
   // basics-constructor contains left object details of json file
@@ -54,12 +60,30 @@ name.textContent=leftside.name;
 left.appendChild(name);
 var email=document.createElement("h2");
 email.textContent=leftside.email;
-left.appendChild(email);
+ left.appendChild(email);
 var ph=document.createElement("p");
 ph.textContent=leftside.phone;
-left.appendChild(ph);
+ left.appendChild(ph);
 var address=document.createElement("h3");
 address.textContent=leftside.address;
-left.appendChild(address);
+ left.appendChild(address);
 console.log(left);
+}
+function education(Educa){
+for(i in Educa)
+{
+var e1=document.createElement("div");
+e1.classList.add("edu2");
+e1.textContent=Educa[i].course;
+var ul=document.createElement("ul");
+for(j in Educa[i].college){
+  var li=document.createElement("li");
+  li.textContent=Educa[i].college[j];
+ul.appendChild(li);
+e1.appendChild(ul);
+edu.appendChild(e1);
+console.log(edu)
+
+}
+}
 }
